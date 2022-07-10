00e1cd6a01b7da373c80a25529a27c5656332216
Gabriel Costa <88682920+gabrielxcosta@users.noreply.github.com>
2022-07-10 15:01:20 -0300

    Delete IMC.py

--- IMC.py
+++
@@ -1,8 +0,0 @@
-# Calculando meu IMC
-nome = 'Gabriel'
-idade = 20
-peso = 60.0 # kg
-altura = 1.71 # m
-IMC = peso / altura ** 2
-classifica = None
-print(nome, 'com', idade, 'anos,', peso, 'quilos e', altura, 'metros de altura tem o IMC:', IMC)
