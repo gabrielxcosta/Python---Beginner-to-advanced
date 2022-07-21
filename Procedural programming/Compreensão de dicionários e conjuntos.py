9b5ba27281589a726071566bc4d7f2e8bdbb808c
Gabriel Costa <88682920+gabrielxcosta@users.noreply.github.com>
2022-07-21 16:05:22 -0300

    Add files via upload

---
+++ Procedural programming/Compreensão de dicionários e conjuntos.py
@@ -0,0 +1,15 @@
+'''
+Compreensão de dicionários e conjuntos em Python
+'''
+
+
+lista = [
+    ('chave1', 'valor1'),
+    ('chave2', 'valor2'),
+    ('chave3', 'valor3')
+]
+
+d1 = {x : y for x, y in lista}
+print('Dicionário:', d1)
+conjunto = {x for x in range(11)}
+print('Conjunto:', conjunto)
