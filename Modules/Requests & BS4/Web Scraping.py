a75045888430257f33a9b1d2733347794bae7c0d
Gabriel Costa <88682920+gabrielxcosta@users.noreply.github.com>
2022-08-29 13:27:16 -0300

    Add files via upload

---
+++ Modules/Requests & BS4/Web Scraping.py
@@ -0,0 +1,15 @@
+'''
+Web Scraping com Python
+'''
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://pt.stackoverflow.com/questions/tagged/python'
+response = requests.get(url)
+html = BeautifulSoup(response.text, 'html.parser')
+
+for question in html.select('.question-summary'):
+    title = question.select_one('.question-hyperlink')
+    date = question.select_one('.relativetime')
+    votes = question.select_one('.vote-count-post')
+    print(date.text)
